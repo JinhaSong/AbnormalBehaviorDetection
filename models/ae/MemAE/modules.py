7510e92848fc84f67ac0ce85d71ab201a74189f8
JinhaSong <jinhasong@sogang.ac.kr>
2024-06-19 21:10:21 +0900

    Create MemAE modules.py

---
+++ models/ae/MemAE/modules.py
@@ -0,0 +1,37 @@
+import torch
+import torch.nn as nn
+
+
+class double_conv(nn.Module):
+    def __init__(self, in_ch, out_ch):
+        super(double_conv, self).__init__()
+        self.conv = nn.Sequential(
+            nn.Conv2d(in_ch, out_ch, 3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(out_ch, out_ch, 3, padding=1),
+            nn.ReLU(inplace=True)
+        )
+
+    def forward(self, x):
+        return self.conv(x)
+
+
+class inconv(nn.Module):
+    def __init__(self, in_ch, out_ch):
+        super(inconv, self).__init__()
+        self.conv = double_conv(in_ch, out_ch)
+
+    def forward(self, x):
+        return self.conv(x)
+
+
+class down(nn.Module):
+    def __init__(self, in_ch, out_ch):
+        super(down, self).__init__()
+        self.mpconv = nn.Sequential(
+            nn.MaxPool2d(2),
+            double_conv(in_ch, out_ch)
+        )
+
+    def forward(self, x):
+        return self.mpconv(x)
