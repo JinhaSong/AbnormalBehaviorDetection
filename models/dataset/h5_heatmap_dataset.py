e08b99d1f95252beab03bd9385799a4d01334c63
JinhaSong <jinhasong@sogang.ac.kr>
2024-06-19 21:07:10 +0900

    Update h5_heatmap_dataset.py - HDF5TestDataset 체거

--- models/dataset/h5_heatmap_dataset.py
+++ models/dataset/h5_heatmap_dataset.py
@@ -5,7 +5,7 @@ from torch.utils.data import Dataset, default_collate
 from torchvision.transforms import Compose, ToTensor, Normalize
 
 
-class HDF5TrainDataset(Dataset):
+class HDF5ShanghaiTechDataset(Dataset):
     def __init__(self, h5_file, transform=None):
         super().__init__()
         self.h5_file = h5_file
@@ -20,6 +20,7 @@ class HDF5TrainDataset(Dataset):
     def __getitem__(self, idx):
         with h5py.File(self.h5_file, 'r') as file:
             video_data = file[self.dataset_keys[idx]]['video_data'][()]
+            label = file[self.dataset_keys[idx]]['label'][()]
 
         video_data = video_data.transpose((1, 0, 2, 3))
         video_data = torch.tensor(video_data, dtype=torch.float32)
@@ -27,62 +28,9 @@ class HDF5TrainDataset(Dataset):
         if self.transform:
             video_data = torch.stack([self.transform(video_data[c]) for c in range(video_data.size(0))])
 
-        return video_data
+        label = torch.tensor(label, dtype=torch.float32)
 
-
-class HDF5TestDataset(Dataset):
-    def __init__(self, h5_file, transform=None, max_frames=128):
-        super().__init__()
-        self.h5_file = h5_file
-        self.transform = transform
-        self.max_frames = max_frames
-        self.dataset_keys = []
-
-        with h5py.File(self.h5_file, 'r') as file:
-            self.dataset_keys = list(file.keys())
-
-    def __len__(self):
-        return len(self.dataset_keys)
-
-    def __getitem__(self, idx):
-        video_key = self.dataset_keys[idx]
-
-        with h5py.File(self.h5_file, 'r') as file:
-            video_data = file[video_key]['video_data'][()]
-            frame_numbers = file[video_key]['frame_numbers'][()]
-            label = file[video_key]['label'][()]
-            annotation = json.loads(file[video_key]['annotation'][()])
-
-        video_data = video_data.transpose((1, 0, 2, 3))
-        video_data = torch.tensor(video_data, dtype=torch.float32)
-
-        # Pad video data to max_frames
-        if video_data.shape[1] < self.max_frames:
-            pad_size = self.max_frames - video_data.shape[1]
-            padding = torch.zeros((video_data.shape[0], pad_size, video_data.shape[2], video_data.shape[3]))
-            video_data = torch.cat((video_data, padding), dim=1)
-
-        if self.transform:
-            video_data = torch.stack([self.transform(video_data[c]) for c in range(video_data.size(0))])
-
-        anomalies = []
-        video_labels = annotation.get('anomalies', [])
-        video_length = annotation.get('video_length', [])
-
-        for anomaly in video_labels:
-            for key, intervals in anomaly.items():
-                anomalies.extend(intervals)
-
-        return {
-            'video_data': video_data,
-            'frame_numbers': frame_numbers,
-            'label': label,
-            'anomalies': anomalies,
-            'video_key': video_key,
-            'video_length': video_length
-        }
-
-
+        return video_data, label
 
 def get_transform():
     return Compose([
