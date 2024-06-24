88c4b58ed123634436f209b64eacbe6c1f6eab71
JinhaSong <jinhasong@sogang.ac.kr>
2024-06-24 14:04:38 +0900

    Update heatmap_c3d_4c.py

--- models/c3d/heatmap_c3d_4c.py
+++ models/c3d/heatmap_c3d_4c.py
@@ -7,7 +7,7 @@ from models.c3d.module.bottleneck3d import Bottleneck3D
 
 
 class ResNet3D(nn.Module):
-    def __init__(self, block, num_blocks, in_channels=32, num_classes=512):
+    def __init__(self, block, num_blocks, in_channels=32):
         super(ResNet3D, self).__init__()
         self.in_channels = 64
 
@@ -45,14 +45,14 @@ class ResNet3D(nn.Module):
         return x
 
 
-def HeatmapC3D4C(depth=18, in_channels=32, num_classes=512):
+def HeatmapC3D4C(depth=18, in_channels=32):
     if depth == 18:
-        return ResNet3D(BasicBlock3D, [2, 2, 2, 2], in_channels, num_classes)
+        return ResNet3D(BasicBlock3D, [2, 2, 2, 2], in_channels)
     elif depth == 34:
-        return ResNet3D(BasicBlock3D, [3, 4, 6, 3], in_channels, num_classes)
+        return ResNet3D(BasicBlock3D, [3, 4, 6, 3], in_channels)
     elif depth == 50:
-        return ResNet3D(Bottleneck3D, [3, 4, 6, 3], in_channels, num_classes)
+        return ResNet3D(Bottleneck3D, [3, 4, 6, 3], in_channels)
     elif depth == 101:
-        return ResNet3D(Bottleneck3D, [3, 4, 23, 3], in_channels, num_classes)
+        return ResNet3D(Bottleneck3D, [3, 4, 23, 3], in_channels)
     elif depth == 152:
-        return ResNet3D(Bottleneck3D, [3, 8, 36, 3], in_channels, num_classes)
+        return ResNet3D(Bottleneck3D, [3, 8, 36, 3], in_channels)
