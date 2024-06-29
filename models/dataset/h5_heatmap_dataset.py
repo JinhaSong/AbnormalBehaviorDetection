c82f55cd701513362ef502fcd7135bfc58af8267
JinhaSong <jinhasong@sogang.ac.kr>
2024-06-30 00:03:57 +0900

    Update h5_heatmap_dataset.py - triplet용 데이터셋 클래스 추가 - 맨 마지막 batch가 크기가 안맞으면 패딩을 하거나 크기에 맞게 크롭 하도록 수정 - 소규모로 테스트 가능하도록 클래스(SmallHDF5TripletDataset) 추가

--- models/dataset/h5_heatmap_dataset.py
+++ models/dataset/h5_heatmap_dataset.py
@@ -1,6 +1,6 @@
-import json
 import h5py
 import torch
+import torch.nn.functional as F
 from torch.utils.data import Dataset, default_collate
 from torchvision.transforms import Compose, ToTensor, Normalize
 
@@ -34,33 +34,73 @@ class HDF5Dataset(Dataset):
 
 
 class HDF5TripletDataset(Dataset):
-    def __init__(self, h5_file, transform=None):
+    def __init__(self, h5_file, max_length, transform=None):
         super().__init__()
         self.h5_file = h5_file
         self.transform = transform
-        self.triplet_keys = []
+        self.max_length = max_length
+
         with h5py.File(self.h5_file, 'r') as file:
-            self.triplet_keys = list(file.keys())
+            self.triplet_info = file['triplet_info'][()]
+            self.videos = [file['videos'][key][:] for key in file['videos']]
+            self.normal_indices = file['normal_indices'][()]
+            self.abnormal_indices = file['abnormal_indices'][()]
 
     def __len__(self):
-        return len(self.triplet_keys)
+        return len(self.triplet_info)
+
+    def pad_or_crop(self, tensor, target_length):
+        current_length = tensor.shape[0]
+        if current_length == target_length:
+            return tensor
+        elif current_length > target_length:
+            return tensor[:target_length]
+        else:
+            padding = (0, 0, 0, 0, 0, 0, 0, target_length - current_length)
+            return F.pad(tensor, padding, 'constant', 0)
 
     def __getitem__(self, idx):
-        with h5py.File(self.h5_file, 'r') as file:
-            group = file[self.triplet_keys[idx]]
-            anchor = group['anchor'][()]
-            positive = group['positive'][()]
-            negative = group['negative'][()]
+        anchor_idx, pos_idx, neg_idx = self.triplet_info[idx]
+
+        anchor = torch.tensor(self.videos[anchor_idx], dtype=torch.float32)
+        positive = torch.tensor(self.videos[pos_idx], dtype=torch.float32)
+        negative = torch.tensor(self.videos[neg_idx], dtype=torch.float32)
+
+        anchor = self.pad_or_crop(anchor, self.max_length)
+        positive = self.pad_or_crop(positive, self.max_length)
+        negative = self.pad_or_crop(negative, self.max_length)
+
+        anchor = anchor.transpose(0, 1).transpose(1, 2).transpose(2, 3)  # (C, T, H, W)
+        positive = positive.transpose(0, 1).transpose(1, 2).transpose(2, 3)  # (C, T, H, W)
+        negative = negative.transpose(0, 1).transpose(1, 2).transpose(2, 3)  # (C, T, H, W)
 
         if self.transform:
-            anchor = self.transform(torch.tensor(anchor, dtype=torch.float32))
-            positive = self.transform(torch.tensor(positive, dtype=torch.float32))
-            negative = self.transform(torch.tensor(negative, dtype=torch.float32))
+            anchor = torch.stack([self.transform(anchor[c]) for c in range(anchor.size(0))])
+            positive = torch.stack([self.transform(positive[c]) for c in range(positive.size(0))])
+            negative = torch.stack([self.transform(negative[c]) for c in range(negative.size(0))])
 
         return anchor, positive, negative
+
+
+class SmallHDF5TripletDataset(HDF5TripletDataset):
+    def __init__(self, h5_file, max_length, transform=None, subset_size=10):
+        super().__init__(h5_file, max_length, transform)
+        self.subset_size = subset_size
+        self.triplet_info = self.triplet_info[:self.subset_size]  # Only keep a subset of the triplet info
+
+    def __len__(self):
+        return len(self.triplet_info)
 
 
 def get_transform():
     return Compose([
         Normalize(mean=[0.5], std=[0.5])
     ])
+
+
+def collate_fn(batch):
+    anchors, positives, negatives = zip(*batch)
+    anchors = torch.stack(anchors)
+    positives = torch.stack(positives)
+    negatives = torch.stack(negatives)
+    return anchors, positives, negatives
