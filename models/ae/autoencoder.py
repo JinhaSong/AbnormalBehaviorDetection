70eafc2c80f06bf0729e119b1d3a3b242325587c
JinhaSong <jinhasong@sogang.ac.kr>
2025-06-23 13:01:35 +0900

    Update autoencoder.py

--- models/ae/autoencoder.py
+++ models/ae/autoencoder.py
@@ -1,48 +1,19 @@
-import torch
 import torch.nn as nn
-import pytorch_lightning as pl
 
-class AutoEncoder(pl.LightningModule):
-    def __init__(self, input_dim=512, latent_dim=128, lr=1e-3):
-        super(AutoEncoder, self).__init__()
-        self.save_hyperparameters()
+
+class Autoencoder(nn.Module):
+    def __init__(self, input_dim, encoding_dim):
+        super(Autoencoder, self).__init__()
         self.encoder = nn.Sequential(
-            nn.Linear(input_dim, 256),
-            nn.ReLU(),
-            nn.Linear(256, 128),
-            nn.ReLU(),
-            nn.Linear(128, latent_dim),
-            nn.ReLU(),
+            nn.Linear(input_dim, encoding_dim),
+            nn.ReLU(True)
         )
         self.decoder = nn.Sequential(
-            nn.Linear(latent_dim, 128),
-            nn.ReLU(),
-            nn.Linear(128, 256),
-            nn.ReLU(),
-            nn.Linear(256, input_dim),
-            nn.ReLU(),
+            nn.Linear(encoding_dim, input_dim),
+            nn.ReLU(True)
         )
-        self.lr = lr
 
     def forward(self, x):
         encoded = self.encoder(x)
         decoded = self.decoder(encoded)
         return decoded
-
-    def training_step(self, batch, batch_idx):
-        x = batch
-        reconstructed = self(x)
-        loss = nn.functional.mse_loss(reconstructed, x)
-        self.log('train_loss', loss)
-        return loss
-
-    def validation_step(self, batch, batch_idx):
-        x = batch
-        reconstructed = self(x)
-        loss = nn.functional.mse_loss(reconstructed, x)
-        self.log('val_loss', loss)
-        return loss
-
-    def configure_optimizers(self):
-        optimizer = torch.optim.Adam(self.parameters(), lr=self.lr)
-        return optimizer
